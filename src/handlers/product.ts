7b3d9860174def5810419273bdaca2826adcb1f5
irenemonzon <irene.monzonm@gmail.com>
2024-05-18 09:35:41 +1000

    create middleware to create products

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,23 +1,15 @@
 import {Request,Response} from 'express'
-import {check,validationResult} from 'express-validator'
 import Product from '../models/Products.model'
 
 export const createProduct=async(req:Request,res:Response)=> {
-    //validacion
-    await check("name").notEmpty().withMessage('El nombre de producto no puede ir vacio').run(req)
 
-    await check("price")
-            .isNumeric().withMessage("Valor no valido")
-            .notEmpty().withMessage('El precio del producto no puede ir vacio')
-            .custom(value=>value >0).withMessage('Precio no valido')
-            .run(req)
+    try{
+        const product=await Product.create(req.body)
+        res.json({data:product})
 
-    let errors=validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()})
+    }catch(error){
+        console.log(error)
+
     }
-
-  const product=await Product.create(req.body)
-   res.json({data:product})
 
 }
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,8 @@
 import {Router } from 'express'
+import {body} from 'express-validator';
 import { createProduct } from './handlers/product'
+import { ExpressValidator } from 'express-validator'
+import { handleInputErrors } from './middleware';
 
 const router=Router()
 
@@ -8,6 +11,16 @@ router.get('/',(req,res)=>{
     res.json('Desde get')
 
 })
-router.post('/',createProduct )
+router.post('/',
+ //validacion
+    body("name")
+    .notEmpty().withMessage('El nombre de producto no puede ir vacio'),
+    body("price")
+            .isNumeric().withMessage("Valor no valido")
+            .notEmpty().withMessage('El precio del producto no puede ir vacio')
+            .custom(value=>value >0).withMessage('Precio no valido'),
+    handleInputErrors,
+    createProduct 
+)
 
 export default router
