8487e6632da2f758017ff7763abd86294c94d18d
irenemonzon <irene.monzonm@gmail.com>
2024-05-24 16:51:44 +1000

    create document to get product by Id and get product with swagger

--- src/router.ts
+++ src/router.ts
@@ -6,7 +6,78 @@ import { handleInputErrors } from './middleware';
 
 const router=Router()
 
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          Product:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      description: The Product ID
+ *                      example: 1
+ *                  name:
+ *                      type: string
+ *                      description: The Product name
+ *                      example: Monitor curvo de 49 pulgadas
+ *                  price:
+ *                      type: number
+ *                      description: The Product price
+ *                      example: 300
+ *                  availability:
+ *                      type: boolean
+ *                      description: The Product availability
+ *                      example: true 
+ */
+/**
+ * @swagger
+ * /api/products:
+ *      get:
+ *          summary: Get a list of product
+ *          tags:
+ *              - Products
+ *          description: Return a list of products
+ *          responses:
+ *              200:
+ *                  description: Succesful response
+ *                  content:
+ *                      application/json:
+ *                              schema:
+ *                                  type: array
+ *                                  items:
+ *                                      $ref: '#/components/schemas/Product'
+ *                                  
+ */
+
 router.get('/',getProducts)
+/**
+ * @swagger
+ * /api/products/{id}:
+ *      get:
+ *          summary: Get a product by ID
+ *          tags:
+ *              - Products
+ *          description: Return a product based on its unique ID
+ *          parameters:
+ *            - in: path
+ *              name: id
+ *              description: The ID of the product to retrieve
+ *              required: true
+ *              schema:
+ *                  type: integer
+ *          responses: 
+ *                  200:
+ *                      description: Successful response
+ *                      content:
+ *                          application/json:
+ *                              schema:
+ *                                  $ref: '#/components/schemas/Product'
+ *                  404:
+ *                      description: Not found
+ *                  400:
+ *                      description: Bad request-Invalid ID
+ */
 
 router.get('/:id',
     param('id').isInt().withMessage('ID no valido'),
