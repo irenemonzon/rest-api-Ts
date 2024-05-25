3f9d14a501c4092daf1c2b560b98934a9fc49420
irenemonzon <irene.monzonm@gmail.com>
2024-05-25 16:39:11 +1000

    document endpoint put, pacth and delete in swagger

--- src/router.ts
+++ src/router.ts
@@ -67,16 +67,16 @@ router.get('/',getProducts)
  *              schema:
  *                  type: integer
  *          responses: 
- *                  200:
- *                      description: Successful response
- *                      content:
- *                          application/json:
- *                              schema:
- *                                  $ref: '#/components/schemas/Product'
- *                  404:
- *                      description: Not found
- *                  400:
- *                      description: Bad request-Invalid ID
+ *              200:
+ *                 description: Successful response
+ *                 content:
+ *                      application/json:
+ *                          schema:
+ *                             $ref: '#/components/schemas/Product'
+ *              404:
+ *                 description: Not found
+ *              400:
+ *                 description: Bad request-Invalid ID
  */
 
 router.get('/:id',
@@ -84,6 +84,38 @@ router.get('/:id',
     handleInputErrors,
     getProductById
 )
+
+/**
+ * @swagger
+ * /api/products:
+ *  post:
+ *      summary: Create a new product
+ *      tags:
+ *          -   Products
+ *      description: Returns a new record in the database
+ *      requestBody: 
+ *             required: true
+ *             content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              name:
+ *                                  type: string
+ *                                  example: "Monitor curvo 49 pulgadas"
+ *                              price:
+ *                                  type: number
+ *                                  example: 399
+ *      responses:
+ *          201:
+ *              description: Successfull response
+ *              content:
+ *                      application/json:
+ *                              schema:
+ *                                      $ref: '#/components/schemas/Product'
+ *          400:
+ *              description: Bad request-invalid input data
+ */
 
 router.post('/',
  //validacion
@@ -97,6 +129,52 @@ router.post('/',
     createProduct 
 )
 
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  put:
+ *     summary: Updates a product with user input
+ *     tags:
+ *          -   Products
+ *     description: Return the updated product
+ *     parameters:
+ *           -  in: path
+ *              name: id
+ *              description: The ID of the product to retrieve
+ *              required: true
+ *              schema:
+ *                  type: integer
+ *     requestBody: 
+ *             required: true
+ *             content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              name:
+ *                                  type: string
+ *                                  example: "Monitor curvo 49 pulgadas"
+ *                              price:
+ *                                  type: number
+ *                                  example: 399
+ *                              availability:
+ *                                  type: boolean
+ *                                  example: true
+ *     responses:
+ *          200:
+ *              description: Succesful response
+ *              content:
+ *                      application/json:
+ *                              schema:
+ *                                      $ref: '#/components/schemas/Product'
+ *          400:
+ *              description: Bad request- Invalid ID or invalid input data
+ *          404:
+ *              description: Product Not Found
+ *              
+ *              
+ */
+
 router.put('/:id',
     param('id').isInt().withMessage('ID no valido'),
     body("name")
@@ -109,12 +187,69 @@ router.put('/:id',
     handleInputErrors,
     updateProduct
 )
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   patch:
+ *      summary: Update Product availability
+ *      tags:
+ *          -   Products
+ *      description: Return update availabiliy
+ *      parameters:
+ *           -  in: path
+ *              name: id
+ *              description: The ID of the product to retrieve
+ *              required: true
+ *              schema:
+ *                  type: integer
+ *      responses:
+ *          200:
+ *              description: Succesful response
+ *              content:
+ *                      application/json:
+ *                              schema:
+ *                                      $ref: '#/components/schemas/Product'
+ *          400:
+ *              description: Bad request- Invalid ID
+ *          404:
+ *              description: Product Not Found
+ *          
+ *      
+ * 
+ */
 
 router.patch('/:id',
     param('id').isInt().withMessage('ID no valido'),
     handleInputErrors,
     updateAvailability
 )
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   delete:
+ *      summary: Deletes a product by a given ID
+ *      tags:
+ *          -   Products
+ *      description: Return a confirmation message
+ *      parameters:
+ *           -  in: path
+ *              name: id
+ *              description: The ID of the product to delete
+ *              required: true
+ *              schema:
+ *                  type: integer
+ *      responses:
+ *          200:
+ *              description: Succesful response
+ *              content:
+ *                 application/json:
+ *                      schema:
+ *                          type: string 
+ *                          value:  'Producto eliminado'
+ *          404:
+ *              description: Product Not Found
+ *          
+ */
 
 
 router.delete('/:id',
